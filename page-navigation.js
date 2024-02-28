d6b489e7eb4667a4d6a8cf546b57ce832834ee31
Chris Hodges <113991354+chodgesfigma@users.noreply.github.com>
2024-02-28 10:17:39 +0000

    Create page-navigation.js

---
+++ page-navigation.js
@@ -0,0 +1,5 @@
+<div class="page-navigation">
+  <img class="icons-arrow-back" src="img/arrow-back-24px.svg" />
+  <div class="header">Choose Payment Method</div>
+  <img class="icons-arrow-back" src="img/arrow-back-24px.png" />
+</div>
